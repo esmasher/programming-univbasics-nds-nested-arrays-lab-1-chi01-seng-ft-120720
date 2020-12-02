cdf2cd01851ede48e92088e0d1d6be46bbd51a8a
Esma Sherwani <esmasherr@gmail.com>
2020-12-02 00:28:06 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -42,6 +42,6 @@ end
   # Return the updated matrix
 
 def matrix_update(matrix, row, column, new_value)
-  assembled_matrix [1][2] = new_value
+  sorted_matrix [1][2] = new_value
   return matrix 
 end
